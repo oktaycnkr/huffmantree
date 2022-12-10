14958b314552b25e1894f6d3106065be2d2cfe08
Oktay Cankir <82322899+oktaycnkr@users.noreply.github.com>
2022-12-10 21:23:15 +0300

    Update Node.java

--- huffman/src/Node.java
+++ huffman/src/Node.java
@@ -1,19 +1,18 @@
 class Node
 {
-    //storing character in ch variable of type character
+    
     Character ch;
-    //storing frequency in freq variable of type int
     Integer freq;
-    //initially both child (left and right) are null
+    
     Node left = null;
     Node right = null;
-    //creating a constructor of the Node class
+    
     Node(Character ch, Integer freq)
     {
         this.ch = ch;
         this.freq = freq;
     }
-    //creating a constructor of the Node class
+    
     public Node(Character ch, Integer freq, Node left, Node right)
     {
         this.ch = ch;
